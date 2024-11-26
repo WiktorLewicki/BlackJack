c875720cccf89fdd9336b047f3ba298a1d6bc191
WiktorLewicki <wiktorek.tfm@gmail.com>
2024-11-26 03:38:47 +0100

    Ogólne skrócenie kodu i dodanie wersji dla linux

--- README.md
+++ README.md
@@ -3,5 +3,9 @@ Link to the presentation: https://www.youtube.com/watch?v=DOnc1WcXy8M&ab_channel
 Work time: 6h
 
 Here is my implementation of BlackJack along with an additional function that suggests the best move. This function is a Monte Carlo method optimized using multithreading.
+The application is in Polish.
 
-To play, you need to run the file /BlackJack-PLAY/BlackJack.exe. The application is in Polish.
+
+For Windows: To play the game with a graphical interface, you need to go to the BlackJack-WIN folder and run the BlackJack.exe file.
+
+For Linux: To play the game, install the required packages listed in packets.txt and use the following command to compile and run the game: bash build.sh
---
+++ src/mainwindow.h
@@ -0,0 +1,44 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QMainWindow>
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+    void resetuj();
+    void pokaz(std::string s, bool who);
+    void update_money();
+    void wygrana();
+    void przegrana();
+    void bet_end();
+    void on_bet_clicked();
+    void remis();
+    void BJ();
+
+    void on_stand_clicked();
+
+    void on_hit_clicked();
+
+    void on_actionAutor_triggered();
+
+    void on_actionZasady_triggered();
+    void on_math_clicked();
+
+    void on_doubledown_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
